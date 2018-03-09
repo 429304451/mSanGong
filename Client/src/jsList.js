67bdeca8084e552272d06776b4cf884dcf4871bd
youchangwei <429304451@qq.com>
2018-03-09 23:38:15 +0800

    client

---
+++ Client/src/jsList.js
@@ -0,0 +1,16 @@
+
+var jsListFramework = [
+
+];
+
+var jsListGame = [
+	"src/resource.js",
+    "src/app.js"
+];
+
+
+
+if (typeof module != "undefined") {
+    module.exports.jsListFramework = jsListFramework;
+    module.exports.jsListGame = jsListGame;
+}
